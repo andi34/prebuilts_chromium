273be0f8f4d23bff9dd4792d8c9eb0ead923cf60
Andreas Blaesius <skate4life@gmx.de>
2018-01-07 20:52:27 +0100

    Cleanup compatible and prevent from issues
    More information upcoming README

--- Android.mk
+++ Android.mk
@@ -3,18 +3,17 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := Chromium
 
-ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 24 || echo 1),)
-# minSdkVersion=24 (Nougat)
-LOCAL_SRC_FILES    := MonochromePublic.apk
-else
-ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 21 || echo 1),)
-# minSdkVersion=21 (Lollipop)
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 23 || echo 1),)
+# minSdkVersion=23 (Marshmallow)
 LOCAL_SRC_FILES    := ChromeModernPublic.apk
-LOCAL_PREBUILT_JNI_LIBS_arm := @lib/armeabi-v7a/crazy.libchrome.so
-LOCAL_PREBUILT_JNI_LIBS_arm += @lib/armeabi-v7a/libchromium_android_linker.so
 else
 # minSdkVersion=16 (Jelly Bean)
 LOCAL_SRC_FILES    := ChromePublic/ChromePublic.apk
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 21 || echo 1),)
+# required for Lollipop
+LOCAL_PREBUILT_JNI_LIBS_arm := @lib/armeabi-v7a/libchrome.so
+LOCAL_PREBUILT_JNI_LIBS_arm += @lib/armeabi-v7a/libchromium_android_linker.so
+else
 LOCAL_REQUIRED_MODULES := libchrome libchromium_android_linker
 endif
 endif
@@ -23,13 +22,10 @@ LOCAL_MODULE_TAGS   := optional
 LOCAL_MODULE_CLASS  := APPS
 LOCAL_CERTIFICATE   := PRESIGNED
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_OVERRIDES_PACKAGES := Browser Browser2
 LOCAL_MULTILIB := 32
 include $(BUILD_PREBUILT)
 
-
 # KitKat Compatibility
-
 ifneq ($(shell test $(PLATFORM_SDK_VERSION) -ge 21 || echo 1),)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libchrome
